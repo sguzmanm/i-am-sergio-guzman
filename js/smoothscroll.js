22b8b0690ce85b6b83937460d96f551d190eee47
Sergio <sguzman@truora.com>
2019-08-15 18:27:50 -0500

    Fixed smooth scroll and added CSS for job timeline, now to work on index with skills and unique JS functionality

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,33 +1,60 @@
 "use strict";
 
-let scrollableClasses = [".nav-link", ".navbar-brand"];
+const activeClass = "active";
+const scrollableClasses = [".nav-link", ".navbar-brand"];
+let links = document.querySelectorAll(scrollableClasses);
 
 document.addEventListener("DOMContentLoaded", function() {
-  scrollTo();
+  setupLinkBehavior();
 });
 
-function scrollTo() {
-  let links = document.querySelectorAll(scrollableClasses);
+function setupLinkBehavior() {
   links.forEach(function(link) {
-    link.onclick = scrollAnchors;
+    link.onclick = setupAnchorBehavior;
   });
 }
 
-function scrollAnchors(e, respond = null) {
+function setupAnchorBehavior(e, respond = null) {
+  setupActiveTab(this);
+  scrollAnchors(this, e, respond);
+}
+
+/**
+ * Setup the active tab link
+ */
+function setupActiveTab(link) {
+  // Remove all active links
+  links.forEach(el => el.classList.remove(activeClass));
+  // Add current active link
+  link.classList.add(activeClass);
+}
+
+/**
+ * @param {} link Link for the onclick function
+ * @param {} e Event of the scroll
+ * @param {*} respond Link from which the page is loaded
+ */
+function scrollAnchors(link, e, respond = null) {
+  console.log("Target", respond ? respond.href : link.getAttribute("href"));
   const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-  // Prevent default link anchoring
-  e.preventDefault();
   // Get target
   var targetID = respond
     ? respond.getAttribute("href")
-    : this.getAttribute("href");
+    : link.getAttribute("href");
   const targetAnchor = document.querySelector(targetID);
   // Early return if no target anchor specified
   if (!targetAnchor) return;
+  // Prevent default link anchoring
+  e.preventDefault();
+
   const originalTop = distanceToTop(targetAnchor);
   // Smooth scroll
-  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
-  // CHeck if done
+  window.scrollBy({
+    top: originalTop,
+    left: 0,
+    behavior: "smooth"
+  });
+  // Check if done
   const checkIfDone = setInterval(function() {
     const atBottom =
       window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
