bf4c6bcc82ec3a2e0b90301cd008ec4ae4857902
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2020-04-28 14:20:00 -0500

    Add trademark image

--- src/components/WorkAndProjects/WorkCard.vue
+++ src/components/WorkAndProjects/WorkCard.vue
@@ -8,10 +8,10 @@
       </div>
       <div class="card__item card__org"
         v-for="organization in work.organizations" :key="`${work.title}_${organization.name}`">
-          <div class="card__img">
+          <div :class="{card__img:!organization.isTrademarkSensitive, card__full_img: organization.isTrademarkSensitive}">
             <img :src="organization.logo" loading="lazy" :alt="`Company logo for ${organization.name}`"/>
           </div>
-          <p>{{organization.name}}</p>
+          <p v-show="!organization.isTrademarkSensitive">{{organization.name}}</p>
       </div>
       <media-preview :media="`work-and-projects/${work.media?work.media:placeholder}`"
         :alt="work.title"/>
@@ -48,6 +48,13 @@ export default {
     MediaPreview,
   },
   setup({ work }) {
+    const orgs = ['Truora'];
+    const isTrademarkSensitive = (organizationName) => {
+      if (!organizationName) { return false; }
+
+      return orgs.includes(organizationName);
+    };
+
     const workDate = computed(() => {
       const options = { year: 'numeric', month: 'long' };
       const startDate = new Date(work.startDate);
@@ -70,41 +77,43 @@ export default {
 
     const placeholder = ref('placeholder.jpeg');
 
-    return { workDate, statusColor, placeholder };
+    return {
+      isTrademarkSensitive, workDate, statusColor, placeholder,
+    };
   },
 };
 </script>
 
 <style scoped>
 .card{
-    display:flex;
-    flex-direction: column;
-    text-align: justify;
-    align-items: center;
-    justify-content: center;
+  display:flex;
+  flex-direction: column;
+  text-align: justify;
+  align-items: center;
+  justify-content: center;
 
-    flex: 1 0 calc(25% - 10px);
-    background-color: var(--background-color);
-    border: 2px solid var(--text-color);
-    color: var(--text-color);
+  flex: 1 0 calc(25% - 10px);
+  background-color: var(--background-color);
+  border: 2px solid var(--text-color);
+  color: var(--text-color);
 
-    margin: 20px;
-    padding:20px;
-    max-width: 80%;
+  margin: 20px;
+  padding:20px;
+  max-width: 80%;
 }
 
 .card__item{
-    display:flex;
-    flex-direction: row;
-    width: 100%;
-    align-items: center;
+  display:flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
 
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .card__item p{
-    padding: 0 20px;
-    font-weight: bold;
+  padding: 0 20px;
+  font-weight: bold;
 }
 
 .card__header{
@@ -112,48 +121,64 @@ export default {
 }
 
 .card__org{
-    justify-content: flex-start;
+  justify-content: flex-start;
 }
 
 .card__img{
-    border-radius: 50%;
-    background-color: white;
-    width: 10%;
-    padding:20px;
-    margin:10px;
+  border-radius: 50%;
+  background-color: white;
+  width: 10%;
+  padding:10px;
+  margin:10px;
+
+  display:flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.card__img img{
-    width: 100%;
+.card__full_img{
+  background-color: white;
+  border-radius: 20px;
+  width: 100%;
+  padding:10px;
+  margin:10px;
+
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card__img img,.card__full_img img{
+  width: 80%;
 }
 
 .card__status{
-    border-radius:5px;
-    font: bold 15px Sans-Serif;
-    text-align: center;
-    text-shadow: rgba(255,255,255,0.5) 0px 1px 0px;
+  border-radius:5px;
+  font: bold 15px Sans-Serif;
+  text-align: center;
+  text-shadow: rgba(255,255,255,0.5) 0px 1px 0px;
 
-    position: relative;
-    padding: 7px 35px;
+  position: relative;
+  padding: 7px 35px;
 
-    -webkit-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
-    -moz-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
-    box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+  -webkit-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+  -moz-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+  box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
 
-    color: var(--background-color);
-    background-color: var(--highlight-color-2);
+  color: var(--background-color);
+  background-color: var(--highlight-color-2);
 }
 
 .card__content{
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .card__content a,a:visited{
-    color: var(--highlight-color);
+  color: var(--highlight-color);
 }
 
 .card__content a:hover{
-    color: var(--highlight-color-2)
+  color: var(--highlight-color-2)
 }
 
 @media (max-width: 700px) {
