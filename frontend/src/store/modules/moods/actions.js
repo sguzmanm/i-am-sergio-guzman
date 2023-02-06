113ec12be424b4a7ac9cd6ede778eec176432ae1
Checho <s.guzmanm98@gmail.com>
2023-02-05 20:13:43 -0500

    Add isntructions for pushing to gh-pages alongside necessary configs

--- frontend/src/store/modules/moods/actions.js
+++ frontend/src/store/modules/moods/actions.js
@@ -1,38 +1,47 @@
+const axios = require("axios")
 
-const axios = require('axios');
+const { setCurrentStyle } = require("@/helpers/layout/styles")
+const moods = require("@/helpers/moods/moods.json")
 
-const { setCurrentStyle } = require('@/helpers/layout/styles');
-const moods = require('@/helpers/moods/moods.json');
+const SET = "set"
+const SET_CURRENT = "setCurrent"
+const SET_ERROR = "setError"
+const LOADING = "loading"
 
-const SET = 'set';
-const SET_CURRENT = 'setCurrent';
-const SET_ERROR = 'setError';
-const LOADING = 'loading';
-
-const CLEAR = 'clear';
+const CLEAR = "clear"
 
 // register check start action
 export const setCurrentMood = (context, mood) => {
-  context.commit(SET_CURRENT, mood);
-  setCurrentStyle(mood);
-};
+  context.commit(SET_CURRENT, mood)
+  setCurrentStyle(mood)
+}
 
 export const clearCurrentMood = (context) => {
-  context.commit(CLEAR);
-};
+  context.commit(CLEAR)
+}
 
 export const fetchMoods = async (context) => {
-  const backendURL = process.env.VUE_APP_BACKEND ? process.env.VUE_APP_BACKEND : 'http://localhost:8080';
-  context.commit(LOADING);
+  const backendURL = process.env.VUE_APP_BACKEND
+    ? process.env.VUE_APP_BACKEND
+    : "http://localhost:8080"
+  context.commit(LOADING)
+
+  const shouldFetch = process.env.SHOULD_FETCH
+    ? process.env.SHOULD_FETCH === "true"
+    : false
+  if (shouldFetch) {
+    context.commit(SET, moods)
+    return
+  }
 
   try {
-    const fetchedMoods = await axios.get(`${backendURL}/moods`);
+    const fetchedMoods = await axios.get(`${backendURL}/moods`)
 
-    context.commit(SET, fetchedMoods.data);
-    context.commit(SET_ERROR, '');
+    context.commit(SET, fetchedMoods.data)
+    context.commit(SET_ERROR, "")
   } catch (e) {
-    console.error('Failed to connect with backend', e);
-    context.commit(SET, moods);
-    context.commit(SET_ERROR, e.message); // TODO: Do something with this error
+    console.error("Failed to connect with backend", e)
+    context.commit(SET, moods)
+    context.commit(SET_ERROR, e.message) // TODO: Do something with this error
   }
-};
+}
